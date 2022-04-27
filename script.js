966781c04690ac3678e63d09ce1293ccae9f81e4
MohammadHajMahmoud <mohammdhasaeen@yahoo.com>
2022-04-27 08:45:27 +0300

    adding local storage
    storage added

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
+const todos=JSON.parse(localStorage.getItem('tasks')) ? JSON.parse(localStorage.getItem('tasks')):[];
+const data= JSON.parse(localStorage.getItem('tasks'))? JSON.parse(localStorage.getItem('tasks')):[];
 document.querySelector("#input").addEventListener("keydown", (event) => {
     if(event.key === "Enter"){
       const input = document.querySelector("#input");
         if(!input.value)
           alert("Please add a Task");
         else
+        todos.push(input.value)
+        localStorage.setItem('tasks',JSON.stringify(todos))
       addItem(input.value);
     } 
   });
-  
+
   document.querySelector("#add_item").addEventListener("click", () => {
     const input = document.querySelector("#input");
       if(!input.value)
           alert("Please add a Task");
 
       else
-    addItem(input.value);
+      todos.push(input.value)
+      localStorage.setItem('tasks',JSON.stringify(todos))
+      addItem(input.value);
   });
   
-  addItem = (input) => {
+
+   addItem = (input) => {
+ 
     const item = document.createElement("div");
     const div = document.createElement("div");
     const text = document.createElement("p");
@@ -39,6 +47,10 @@ document.querySelector("#input").addEventListener("keydown", (event) => {
     item.appendChild(text);
     item.appendChild(div);
     item.appendChild(editBtn);
+
     document.querySelector("#to_do_list").appendChild(item);
     document.querySelector("#input").value = "";
   }
+  data.map((todo)=>{
+    addItem(todo);
+  })
